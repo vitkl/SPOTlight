8c1e28e6ee8c55a80359f1b72c7c5a179cb87c5a
MarcElosua <elosua.marc@gmail.com>
2020-09-01 15:53:32 +0200

    updated seed_init_mtrx_nmf_fun.R and train_nmf.R to take raw count matrix

--- R/seed_init_mtrx_nmf_fun.R
+++ R/seed_init_mtrx_nmf_fun.R
@@ -1,7 +1,6 @@
 #' This functions seeds initialization matrices H and W to perform NMF
 #'
-#' @param cluster_markers Object of class dataframe obtained from the function Seurat::FindAllMarkers().
-#' @param se_obj Object of class Seurat with the data of interest.
+#' @param counts_sc Object of class Seurat with the data of interest.
 #' @param ntop Object of class "numeric" or NULL; number of unique markers per cluster used to seed the model, by default NULL. If NULL it uses all of them.
 #' @return This function returns a list with the initialized matrices H and W.
 #' @export
@@ -10,16 +9,16 @@
 
 
 seed_init_mtrx_nmf <- function(cluster_markers,
-                               se_obj,
+                               counts_sc,
                                clust_vr,
                                ntop = NULL) {
 
   #### Get dataset ready ####
-  se_nmf_ready <- prep_seobj_topic_fun(se_obj = se_obj)
+  # Comment because the matrix is already sparse
+  # se_nmf_ready <- prep_seobj_topic_fun(se_obj = se_obj)
 
   # Rank of the model equals the number of cell types
-  k <- length(unique(se_obj@meta.data[, clust_vr]))
-
+  k <- length(unique(clust_vr))
 
   # Select all marker genes for each cluster AND compute their Z score
   if (is.null(ntop)) ntop <- max(table(cluster_markers$cluster))
@@ -39,16 +38,14 @@ seed_init_mtrx_nmf <- function(cluster_markers,
   # Set seedwords from top markers. Here we are setting the weights for each topic, the words that are weighted positively are those belonging to the list of top markers for a cluster.
   # In the seedgenes matrix each row represents a topic and each column represents a gene.
 
-  # To the LDA model we need to pass a matrix with k rows and ngene columns, where each cell has the weight of that gene for that topic. The weight we're assigning is the logFC
-
   # initialize matrix
-  seedgenes <- matrix(nrow = k, ncol = ncol(se_nmf_ready), data = 1e-10)
-  colnames(seedgenes) <- colnames(se_nmf_ready)
+  seedgenes <- matrix(nrow = k, ncol = ncol(counts_sc), data = 1e-10)
+  colnames(seedgenes) <- colnames(counts_sc)
 
   # Add seeds to model, if a cluster-topic has 0 unique markers its row will be set to all 0
   for (i in seq_len(k)) {
     # print(i)
-    clust_row <- cluster_markers_uniq$cluster == as.character(unique(se_obj@meta.data[, clust_vr])[[i]])
+    clust_row <- cluster_markers_uniq$cluster == as.character(unique(clust_vr)[[i]])
     seedgenes[i, as.character(cluster_markers_uniq[clust_row, "gene"])] = cluster_markers_uniq[clust_row, "weight"]
   }
   W <- t(seedgenes)
@@ -57,17 +54,18 @@ seed_init_mtrx_nmf <- function(cluster_markers,
   ###################
   H <- matrix(data = 1e-10,
               nrow = k,
-              ncol = nrow(se_nmf_ready))
+              ncol = nrow(counts_sc))
 
-  for (i in seq_len(nrow(se_nmf_ready))) {
-    h_row <- which(unique(se_obj@meta.data[, clust_vr]) == se_obj@meta.data[i, clust_vr])
+  for (i in seq_len(length(clust_vr))) {
+    # h_row <- which(unique(clust_vr) == se_obj@meta.data[i, clust_vr])
+    h_row <- clust_vr[[i]]
     H[h_row, i] <- 1
   }
 
-  rownames(W) <- rownames(se_obj@assays$RNA@counts)
+  rownames(W) <- rownames(counts_sc)
 
   # get matrix H
-  colnames(H) <- colnames(se_obj@assays$RNA@counts)
+  colnames(H) <- colnames(counts_sc)
 
   return(list("W" = W, "H" = H))
 }
--- R/train_nmf.R
+++ R/train_nmf.R
@@ -1,34 +1,30 @@
 #' This functions carries out the NMF and returns and NMF object.
 #'
 #' @param cluster_markers Object of class dataframe obtained from the function Seurat::FindAllMarkers()
-#' @param se_sc Object of class Seurat with the scRNAseq data.
+#' @param counts_sc Object of class Seurat with the scRNAseq data.
 #' @param mtrx_spatial Object of class Matrix of shape GENESxSPOT.
 #' @param transf Transformation to normalize the count matrix: cpm (Counts per million), uv (unit variance), raw (no transformation applied). By default CPM.
 #' @param ntop Object of class "numeric" or NULL; number of unique markers per cluster used to seed the model, by default NULL If NULL it uses all of them.
 #' @param clust_vr Object of class character; Name of the variable containing the cell clustering.
 #' @param method Object of class character; Type of method to us to find W and H. Look at NMF package for the options and specifications, by default nsNMF.
 #' @param hvg Object of class numeric or "uns"; Number of highly variable genes to use on top of the marker genes, if "uns" then it is completely unsupervised and use top 3000 HVG.
-#' @param assay Object of class character; From which assay to grab the expression data to train the model, by default "RNA".
-#' @param slot Object of class character; From which slot to grab the expression data to train the model, by default "counts".
 #' @return This function returns a list with the initialized matrices H and W.
 #' @export
 #' @examples
 #'
 
 train_nmf <- function(cluster_markers,
-                      se_sc,
-                      mtrx_spatial,
+                      counts_sc,
+                      counts_sp,
                       clust_vr,
                       ntop = NULL,
                       transf = "cpm",
                       method = "nsNMF",
-                      hvg = 3000,
-                      assay = "RNA",
-                      slot = "counts") {
+                      hvg = 3000) {
 
   # Check variables
   if (!is.data.frame(cluster_markers)) stop("ERROR: cluster_markers_all must be a data frame object returned from Seurat::FindAllMarkers()!")
-  if (is(se_sc) != "Seurat") stop("ERROR: se_obj must be a Seurat object!")
+  # if (is(se_sc) != "Seurat") stop("ERROR: se_obj must be a Seurat object!")
   if (!is.character(clust_vr)) stop("ERROR: clust_vr must be a character string!")
   # if (!is(mtrx_spatial, "Matrix")) stop("ERROR: mtrx_spatial must be a matrix object!")
   if (!(is.numeric(ntop) | is.null(ntop))) stop("ERROR: ntop must be numeric or NULL!")
@@ -46,28 +42,25 @@ train_nmf <- function(cluster_markers,
   print("Preparing Gene set")
   # Only train the model with genes shared between the scRNAseq and spatial data
   ## Remove rows with all gene counts 0
-  mtrx_sc <- as.matrix(Seurat::GetAssayData(se_sc,
-                                            assay = assay,
-                                            slot = slot))
-  genes_0_sc <- which(! rowSums(mtrx_sc == 0) == ncol(mtrx_sc))
-  se_sc <- se_sc[genes_0_sc, ]
+  genes_0_sc <- which(! rowSums(counts_sc == 0) == ncol(counts_sc))
+  counts_sc <- counts_sc[genes_0_sc, ]
 
-  genes_0_sp <- which(! rowSums(as.matrix(mtrx_spatial) == 0) == ncol(mtrx_spatial))
-  mtrx_spatial <- mtrx_spatial[genes_0_sp, ]
+  genes_0_sp <- which(! rowSums(as.matrix(counts_sp) == 0) == ncol(counts_sp))
+  counts_sp <- counts_sp[genes_0_sp, ]
 
   ## Remove non intersecting genes from the scRNAseq data
-  genes_spatial <- rownames(mtrx_spatial)
-  genes_sc <- rownames(Seurat::GetAssayData(se_sc,
-                                            assay = assay,
-                                            slot = slot))
+  genes_spatial <- rownames(counts_sp)
+  # genes_sc <- rownames(Seurat::GetAssayData(se_sc,
+  #                                           assay = assay,
+  #                                           slot = slot))
 
   if (length(intersect(genes_sc, genes_spatial)) < 10) stop("Not enough genes in common between the single-cell and mixture dataset.")
-  se_sc <- se_sc[intersect(genes_sc, genes_spatial), ]
+  mtrx_sc <- counts_sc[intersect(genes_sc, genes_spatial), ]
 
   # Update mtrx_sc with the intersecting genes only
-  mtrx_sc <- as.matrix(Seurat::GetAssayData(se_sc,
-                                            assay = assay,
-                                            slot = slot))
+  # mtrx_sc <- as.matrix(Seurat::GetAssayData(se_sc,
+  #                                           assay = assay,
+  #                                           slot = slot))
 
   ## Remove non intersecting genes from the marker list
   cluster_markers <- cluster_markers[cluster_markers$gene %in% rownames(se_sc), ]
@@ -80,8 +73,8 @@ train_nmf <- function(cluster_markers,
 
   } else if (transf == "uv") {
     count_mtrx_t <- scale(t(mtrx_sc),
-                        center = FALSE,
-                        scale = apply(mtrx_sc, 1, sd, na.rm = TRUE))
+                          center = FALSE,
+                          scale = apply(mtrx_sc, 1, sd, na.rm = TRUE))
     count_mtrx <- t(count_mtrx_t)
 
   } else if (transf == "raw") {
@@ -89,7 +82,7 @@ train_nmf <- function(cluster_markers,
   }
 
   # Rank of the model equals the number of cell types
-  k <- length(unique(se_sc@meta.data[, clust_vr]))
+  k <- length(unique(clust_vr))
 
   #################################
   ###### Initialize the model #####
@@ -103,7 +96,7 @@ train_nmf <- function(cluster_markers,
     print("Seeding initial matrices")
     # Get init seeding matrices
     init_mtrx <- seed_init_mtrx_nmf(cluster_markers = cluster_markers,
-                                    se_obj = se_sc,
+                                    counts_sc = count_mtrx,
                                     ntop = ntop,
                                     clust_vr = clust_vr)
     # Initialize the matrix with the seeded matrices
@@ -126,6 +119,6 @@ train_nmf <- function(cluster_markers,
   total_t <- round(difftime(Sys.time(), start_t, units = "mins"), 2)
   print(sprintf("Time to train NMF model was %smins", total_t))
 
-  return(list(nmf_mod, as.vector(se_sc@meta.data[, clust_vr])))
+  return(list(nmf_mod, clust_vr))
 }
 
